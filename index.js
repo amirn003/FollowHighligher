4a16ddd4351e04ff5e9792a2b7c7d0352483a5c0
amirn003 <amir.naar@gmail.com>
2023-11-17 16:08:38 +0100

    add highlight link dynamic

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ highlight.classList.add('highlight');
 document.body.append(highlight);
 
 function highlightLink() {
-  console.log(this);
+  // console.log(this);
+  const linkCoords = this.getBoundingClientRect();
+  console.log(linkCoords);
+  highlight.style.width = `${linkCoords.width}px`;
+  highlight.style.height = `${linkCoords.height}px`;
+  highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
+
 }
 
 triggers.forEach((trigger) => {
